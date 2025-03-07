4b783cb93630b9e4b26649af500a15376fcf6381
Emanuele Coricciati <emanuelecoricciati@libero.it>
2025-03-07 17:57:46 +0100

    chore: add RouteAnalyzer and uber h3 library

---
+++ RouteAnalyzer/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+    id("com.gradleup.shadow") version "8.3.5"
+}
+
+group = "it.polito.wa2.g20"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+dependencies {
+    implementation("com.uber:h3:4.1.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("it.polito.wa2.g20.MainKt")
+}
